394b72bbe1ad7ac4c49911b093537e869df5d098
Robert Brunhage <robertbrunhage@gmail.com>
2020-01-26 07:43:51 +0100

    Add weather card

--- lib/ui/pages/home/widgets/current_weather.dart
+++ lib/ui/pages/home/widgets/current_weather.dart
@@ -12,9 +12,53 @@ class CurrentWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FittedBox(
-      child: Text(
-        '${openWeather.temperature}',
-        style: Theme.of(context).textTheme.display4,
+      child: Container(
+        width: 800,
+        height: 340,
+        child: Card(
+          elevation: 18,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 24,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          '${openWeather.celcius}',
+                          style: Theme.of(context).textTheme.display4,
+                        ),
+                        Text(
+                          '${openWeather.farenheit}',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.wb_sunny,
+                      size: 112,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+                Text(
+                  '${openWeather.generateText()}',
+                  style: Theme.of(context).textTheme.title.copyWith(
+                        color: Colors.grey.shade700,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/view_models/weather_view.dart
+++ lib/view_models/weather_view.dart
@@ -2,10 +2,27 @@ import 'package:open_weather_app/models/open_weather.dart';
 
 class OpenWeatherView {
   OpenWeatherView(OpenWeather openWeather) {
-    temperature = '${openWeather?.main?.temp} °C';
+    temperature = openWeather?.main?.temp;
+    celcius = '${openWeather?.main?.temp}°C';
+    farenheit = '${(openWeather?.main?.temp ?? 0) * 9 / 5 + 32}°F';
   }
 
-  String temperature;
+  double temperature;
+  String celcius;
+  String farenheit;
+
+  String generateText() {
+    final temp = temperature;
+    if (temp < 0) {
+      return "It's cold :)";
+    } else if (temp == 0) {
+      return "It's the magical ZERO";
+    } else if (temp > 0 && temp < 15) {
+      return "It's creeping up";
+    } else {
+      return "It's hot outside now :))";
+    }
+  }
 
   @override
   bool operator ==(Object other) =>
